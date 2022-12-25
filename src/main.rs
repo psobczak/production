ae3ed6bec552d194c5bdb704c16ce6e5376f5277
Paweł Sobczak <pawel.sobczak0@gmail.com>
2022-12-25 23:05:48 +0100

    finished db init script

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use std::net::TcpListener;
+
+use production::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    run(listener)?.await
+}
