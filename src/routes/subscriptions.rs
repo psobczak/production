13505ae2ad01e6fa57ef003efd5ecac6b3eeacf7
Paweł Sobczak <pawel.sobczak0@gmail.com>
2022-12-28 21:28:07 +0100

    collecting logs with tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use serde::Deserialize;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -10,7 +11,20 @@ pub struct FormData {
     email: String,
 }
 
-pub async fn subscriptions(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+
     let query = sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -22,12 +36,17 @@ pub async fn subscriptions(form: web::Form<FormData>, pool: web::Data<PgPool>) -
         Utc::now()
     )
     .execute(pool.as_ref())
+    .instrument(query_span)
     .await;
 
     match query {
         Ok(_) => HttpResponse::Created().finish(),
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,17 @@
-use crate::routes::{health_check, subscriptions};
+use crate::routes::{health_check, subscribe};
 
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpServer};
+use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
 use sqlx::PgPool;
 
 pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
     let connection = web::Data::new(connection);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscriptions))
+            .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection.clone())
     })
     .listen(listener)?
