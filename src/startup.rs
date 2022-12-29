7cae321b3354c32face2622a897e5a0e8d944b0d
Paweł Sobczak <pawel.sobczak0@gmail.com>
2022-12-29 16:33:27 +0100

    secrets

--- src/startup.rs
+++ src/startup.rs
@@ -2,14 +2,15 @@ use crate::routes::{health_check, subscribe};
 
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
+use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
     let connection = web::Data::new(connection);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection.clone())
