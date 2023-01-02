6fe0849d4411bc579e90619c90662a52b3201330
Paweł Sobczak <pawel.sobczak0@gmail.com>
2023-01-02 22:32:58 +0100

    local and production cofiguration

--- src/configuration.rs
+++ src/configuration.rs
@@ -6,8 +6,8 @@ use crate::domain::SubscriberEmail;
 #[derive(Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
     pub email_client: EmailClientSettings,
+    pub application: ApplicationSettings,
 }
 
 #[derive(Deserialize)]
@@ -17,6 +17,12 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+}
+
+#[derive(Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
 }
 
 #[derive(Deserialize)]
@@ -63,12 +69,53 @@ impl DatabaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
+
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+
+    let environment_filename = format!("{}.yaml", environment.as_str());
+
     let settings = config::Config::builder()
-        .add_source(config::File::new(
-            "configuration.yaml",
-            config::FileFormat::Yaml,
+        .add_source(config::File::from(
+            configuration_directory.join("base.yaml"),
+        ))
+        .add_source(config::File::from(
+            configuration_directory.join(&environment_filename),
         ))
         .build()?;
 
     settings.try_deserialize()
+}
+
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not supported environemtn. Use either 'local' or 'prduction'",
+                other
+            )),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
-use std::net::TcpListener;
+use sqlx::{postgres::PgPoolOptions, PgPool};
+use std::{net::TcpListener, time::Duration};
 
 use production::{configuration, email_client::EmailClient, startup, telemetry};
 
@@ -9,24 +9,33 @@ async fn main() -> std::io::Result<()> {
     let subscriber = telemetry::get_subscriber("production".into(), "info".into(), std::io::stdout);
     telemetry::init_subscriber(subscriber);
 
-    let config = configuration::get_configuration().expect("Failed to read configuration file");
-    let connection = PgPool::connect(config.database.connection_string().expose_secret())
-        .await
+    let configuration =
+        configuration::get_configuration().expect("Failed to read configuration file");
+
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(Duration::from_secs(2))
+        .connect_lazy(configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres");
 
-    let sender_email = config
+    let sender_email = configuration
         .email_client
         .sender()
         .expect("Invalid sender email address");
 
     let email_client = EmailClient::new(
-        config.email_client.url().expect("Faile to parse URL"),
+        configuration
+            .email_client
+            .url()
+            .expect("Faile to parse URL"),
         sender_email,
-        config.email_client.authorization_token,
+        configuration.email_client.authorization_token,
     );
 
-    let address = format!("127.0.0.1:{}", config.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
 
-    startup::run(listener, connection, email_client)?.await
+    startup::run(listener, connection_pool, email_client)?.await
 }
